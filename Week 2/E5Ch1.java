ea5317e2a603589809ff2caa9a99f3b7cdf327f3
Martina Karalliu <martinakaralliu03@gmail.com>
2022-09-24 15:45:48 +0200

    work done
    (I guess)

--- Week 2/E5Ch1.java
+++ Week 2/E5Ch1.java
@@ -1,4 +1,11 @@
-
+/*
+ * I coulnd't find the excercise so I did the book example
+ */
+import javax.swing.JOptionPane;
 public class E5Ch1 {
+    public static void main(String[] arg){
+        int creditDays = 30;
+        JOptionPane.showMessageDialog(null, "Credit is due in " + creditDays + " Days.");
+    }
     
 }
