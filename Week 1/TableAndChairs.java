43707981fcfe87dfb8d671f573e23308477e0026
Martina Karalliu <martinakaralliu03@gmail.com>
2022-09-24 14:27:01 +0200

    Changes
    Week 1 Done, Week 2 init

---
+++ Week 1/TableAndChairs.java
@@ -0,0 +1,22 @@
+/*
+ * Description: Write, Compile, and test a class that displays the following pattern to the screen:
+ * x                x
+ * x                x
+ * x    xxxxxxxx    x
+ * xxxx x      x xxxx
+ * x  x x      x x  x
+ * x  x x      x x  x   
+ * 
+ * Save the class as TableAndChairs.java
+ */
+
+public class TableAndChairs{
+    public static void main(String[] arg){
+        System.out.println("x                x");
+        System.out.println("x                x");
+        System.out.println("x    xxxxxxxx    x");
+        System.out.println("xxxx x      x xxxx");
+        System.out.println("x  x x      x x  x");
+        System.out.println("x  x x      x x  x");
+    }
+}
---
+++ Week 2/E5Ch1.java
@@ -0,0 +1,4 @@
+
+public class E5Ch1 {
+    
+}
