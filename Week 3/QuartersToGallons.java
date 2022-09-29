51d75c86efe827efa353984421bbd1a836d753cf
Martina Karalliu <martinakaralliu03@gmail.com>
2022-09-29 21:50:13 +0200

    init

---
+++ Week 3/QuartersToGallons.java
@@ -0,0 +1,14 @@
+/*
+ * Description:
+ * Write a Java class that declares a named constant 
+ * to hold the number of quarts
+ * in a gallon. Also declare a variable to represent
+ * the number of quarts needed for a painting job, 
+ * and assign an appropriate value- for exapmple, 18. 
+ * Compute and display the number of gallons and quarts needed 
+ * for the job. Display explanatory text with the values
+ * - for example, A job that needs 18 quarts requires 4 gallons plus 2 quarts.
+ */
+public class QuartersToGallons {
+    
+}
