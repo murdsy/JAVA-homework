f826534114190937bfe7cd8008c6abed61b13213
Martina Karalliu <martinakaralliu03@gmail.com>
2022-10-07 23:21:23 +0200

    Create pyetesori.java

---
+++ Week 4/pyetesori.java
@@ -0,0 +1,10 @@
+/*
+ * Desc (AL):Shkruani nje program qe shfaq ne menyre te njepasnjeshme 5 dritare konfirmimi me 5 pyetje:
+A ndryshon gjate ekzekutimit te programit nje konstante?
+A mund te perdoret tipi boolean per te ruajtur vlerat true dhe false?
+A mund te perdoret metoda showMessageDialog() per te lexuar nga tastiera?
+A eshte e nevojshme te perdorim metodat parseInt() dhe parseDouble() nese duam te lexojme numra me GUI?
+A mund te ruhet nje program ne java ne nje file me emertim te cfaredoshem, qe nuk ka lidhje me emrin e klases?
+Ne fund duhet te afishohet numri i pikeve te sakta. 
+ */
+
