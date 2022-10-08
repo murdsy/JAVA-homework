bfa3a4a568e1fe9794ac30817196ee7465311f3d
Martina Karalliu <martinakaralliu03@gmail.com>
2022-10-09 00:58:37 +0200

    more homework

---
+++ Week 4/maxn.java
@@ -0,0 +1,6 @@
+/*
+ * Shkruani nje program qe lexon nga tastiera n numra te plote dhe gjen maksimumin e tyre. Programi te nderpritet kur jepet nga tastiera numri -1.\
+ */
+public class maxn {
+    
+}
--- Week 4/rroganet.java
+++ Week 4/rroganet.java
@@ -1,4 +1,26 @@
+/*
+ * Shkruani nje program qe lexon rrogen bruto dhe interesin dhe afishon 
+ * rrogen neto. Programi te krijohet per afishimin ne console dhe me pas ne GUI.
+ */
 import javax.swing.JOptionPane;
+import java.util.Scanner;
+
 public class rroganet {
+    int rrogabruto;
+    float perqindjaInteresit;
+    float rroganet;
+
+    Scanner lol = new Scanner(System.in);
     
+    System.out.println("Vendosni rrogen tuaj bruto: ");
+    rrogabruto = lol.nextInt();
+    System.out.println("Vendosni perqindjen e interesit: ");
+    perqindjaInteresit = lol.nextFloat();
+    
+    lol.close();
+
+    rroganet = (1-(perqindjaInteresit/100))*rrogabruto;
+
+    JOptionPane.showMessageDialog(null, "Rroga juaj neto eshte: "+rroganet );
+
 }
