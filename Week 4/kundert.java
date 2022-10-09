8c686a828315d68705665d3db689969d31155a58
Martina Karalliu <martinakaralliu03@gmail.com>
2022-10-09 17:45:36 +0200

    Nr tek done !!

---
+++ Week 4/kundert.java
@@ -0,0 +1,7 @@
+/*
+ * Shkruani nje program ne Java qe lexon nje numer te plote nga tastiera 
+ * dhe afishon numrin e kundert. Psh nese japim 123456 te afishohet 654321.
+ */
+public class kundert {
+    
+}
