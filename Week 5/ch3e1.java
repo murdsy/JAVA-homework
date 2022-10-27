db93a878cfdcdedad01f68a11b3b7fba832b114d
Martina Karalliu <martinakaralliu03@gmail.com>
2022-10-27 21:01:12 +0200

    W5 first commit

---
+++ Week 5/ch3e1.java
@@ -0,0 +1,3 @@
+public class ch3e1 {
+    
+}
---
+++ Week 5/detyreklase.java
@@ -0,0 +1,14 @@
+
+class detyreklase {
+    public static void Emri(String emri){
+        System.out.println("Emri juaj eshte: "+ emri);
+    }
+
+    public static void Adresa(String adresa){
+        System.out.println("Adresa juaj eshte: "+ adresa);
+    }
+
+    public static void main(String[] args){
+
+    }
+}
