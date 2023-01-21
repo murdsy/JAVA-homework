0505ceccb3f82b29e73556500eff774f49e7c8fb
Martina Karalliu <martinakaralliu03@gmail.com>
2023-01-21 22:14:33 +0100

    done

--- Week 14/ThreeLetterAcronym.java
+++ Week 14/ThreeLetterAcronym.java
@@ -7,10 +7,18 @@
  * three-letter acronym in all uppercase letters. If the user enters more than three words, 
  * ignore the extra words. Save the file as ThreeLetterAcronym.java.
  */
-
+import java.util.Scanner;
 public class ThreeLetterAcronym {
     public static void main(String[] arg){
         //get input first
         //skam bere shume update so gotta do this lol
+        System.out.print("Enter three words: ");
+        String s1, s2, s3;
+        Scanner in = new Scanner(System.in);
+        s1 = in.nextLine();
+        s2 = in.nextLine();
+        s3 = in.nextLine();
+        in.close();
+        System.out.println(s1.charAt(0)+""+s2.charAt(0)+""+s3.charAt(0));
     }
 }
