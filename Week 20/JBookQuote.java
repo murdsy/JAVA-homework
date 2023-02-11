5b99084a48a38a56780b889d9c8e9f5240adb750
Martina Karalliu <martinakaralliu03@gmail.com>
2023-02-11 20:37:20 +0100

    Add some work + some progress

---
+++ Week 20/JBookQuote.java
@@ -0,0 +1,16 @@
+/*
+ * Write an application that displays a JFrame 
+ * containing the opening sentence or two from your 
+ * favorite book. Save the file as JBookQuote.java.
+ */
+import javax.swing.*;
+public class JBookQuote {
+    public static void main(String[] args){
+        JFrame frame = new JFrame("Book Quotes");
+        frame.setSize(420,420); //x,y
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setVisible(true);
+    }
+    
+}
---
+++ Week 20/JBookQuote2.java
@@ -0,0 +1,9 @@
+/*
+ *  Add a button to the frame in the JBookQuote program.
+ *  When the user clicks the button, display the title 
+ * of the book that contains the quote. Save the file as JBookQuote2.java
+ */
+import javax.swing.*;
+public class JBookQuote2 {
+    
+}
