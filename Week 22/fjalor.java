5be10d90b34a4466879f73264871b9dc7499bdea
Martina Karalliu <martinakaralliu03@gmail.com>
2023-02-25 18:36:35 +0100

    hw done

--- Week 22/fjalor.java
+++ Week 22/fjalor.java
@@ -19,9 +19,43 @@ public class fjalor {
         fjalorAS.put("pencil", "laps");
         fjalorAS.put("paper", "flete");
         fjalorAS.put("dog", "qen");
+        fjalorAS.put("cat", "mace");
+        fjalorAS.put("classroom", "klase");
 
+        JFrame frame = new JFrame("fjalor");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(200, 250);
+        JPanel panel = new JPanel();
+        JTextField ang = new JTextField(10);
+        JTextField shqp = new JTextField(10);
+        JLabel l1 = new JLabel("Anglisht");
+        JLabel l2 = new JLabel("Shqip");
+        JButton b = new JButton("Perkthe");
+
+        b.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                if(fjalorAS.containsKey(ang.getText()) == true){
+                    shqp.setText(fjalorAS.get((ang.getText())));
+                }
+                else {
+                    shqp.setText("Error: nuk ka fjale");
+                }
+            }
+        });
         
+        panel.add(l1);
+        panel.add(ang);
+        panel.add(b);
+        panel.add(l2);
+        panel.add(shqp);
+
+        frame.add(panel);
+        frame.setVisible(true);
+
     }
+
+
     
 
 }
