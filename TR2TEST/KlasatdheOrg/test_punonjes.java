6b449f1bd11716cf922fc35285fa8cf0448782b5
Martina Karalliu <martinakaralliu03@gmail.com>
2023-02-26 14:27:50 +0100

    test

--- TR2TEST/KlasatdheOrg/test_punonjes.java
+++ TR2TEST/KlasatdheOrg/test_punonjes.java
@@ -1,3 +1,17 @@
 public class test_punonjes {
-    
+    public static void main(String[] args){
+        punonjes p = new punonjes();
+        p.setEmri("andi");
+        p.setMbiemri("gjati");
+        p.setRroga(345555.5);
+        p.setNumri(69);
+
+        System.out.println("Punonjesi "
+	    	+p.getEmri()+" "
+	    	+p.getMbiemri()+" ka kodin "
+	    	+p.getNumri()+" dhe rrogen "
+	    	+p.getRroga());
+
+        
+    }
 }
