dccb60ad3ed327f001e4801210ddfc23ae2d2973
Martina Karalliu <martinakaralliu03@gmail.com>
2023-02-26 14:24:40 +0100

    stil working

--- TR2TEST/KlasatdheOrg/punonjes.java
+++ TR2TEST/KlasatdheOrg/punonjes.java
@@ -1,3 +1,46 @@
 public class punonjes{
     private int numri;
+    private String emri;
+    private String mbiemri;
+    private double rroga;
+
+    public int getNumri(){
+        return numri;
+    }
+
+    public void setNumri(int n){
+        numri = n;
+    }
+
+    public String getMbiemri(){
+        return mbiemri;
+    }
+
+    public void setMbiemri(String m){
+        mbiemri = m;
+    }
+
+    public String getEmri(){
+        return emri;
+    }
+
+    public void setEmri(String e){
+        emri=e;
+    }
+
+    public double getRroga(){
+        return rroga;
+    }
+
+    public void setRroga(double r){
+        rroga=r;
+    }
+
+    public static void main(String[] args){
+        punonjes punonjsi = new punonjes();
+
+        punonjsi.setEmri("Bob");
+        punonjsi.setMbiemri("rrizi");
+        System.out.println(">> "+punonjsi.getEmri()+" "+punonjsi.getMbiemri());
+    }
 }
