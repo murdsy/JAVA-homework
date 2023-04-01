d869851d2c4d2e6a21b6a62e74d68fa2a187e2c8
Martina Karalliu <martinakaralliu03@gmail.com>
2023-04-01 20:11:34 +0200

    done

--- Week 28/JColorFrame.java
+++ Week 28/JColorFrame.java
@@ -4,25 +4,63 @@ other regions. Save the file as JColorFrame.java.*/
 
 import java.awt.*;
 import java.awt.event.*;
-import java.awt.color.*;
+import java.util.Random;
 import javax.swing.*;
 
 public class JColorFrame extends JFrame implements ActionListener{
     private JFrame frame = new JFrame();
     private JButton button = new JButton();
+    //private Color color = new Color();
+    JPanel n = new JPanel();
+    JPanel w = new JPanel();
+    JPanel east = new JPanel();
+    JPanel s = new JPanel();
+   
+    Random rand = new Random();
+    final int pHeight  = 50, pWidth = 150; 
 
     public JColorFrame(){
         super();
-        frame.setLayout(new BorderLayout());
+        frame.setLayout(new BorderLayout( 50, 50));
+        //button.setPreferredSize(new Dimension(150,50));
         add(button, BorderLayout.CENTER);
+        add(n, BorderLayout.NORTH);
+        add(w, BorderLayout.WEST);
+        add(east, BorderLayout.EAST);
+        add(s, BorderLayout.SOUTH);
+        
         button.addActionListener(this);
     }
 
     public void actionPerformed(ActionEvent e){
-        
+        //1 - north, 2-west, 3-east, 4-south
+        int position = rand.nextInt(4) + 1;
+        int red = rand.nextInt(255);
+        int green = rand.nextInt(255);
+        int blue = rand.nextInt(255);
+
+        switch(position){
+            case 1:
+                n.setBackground(new Color(red,green,blue));
+                break;
+            case 2:
+                w.setBackground(new Color(red,green,blue));
+                break;
+            case 3:
+                east.setBackground(new Color(red,green,blue));
+                break;
+            case 4:
+                s.setBackground(new Color(red,green,blue));
+                break;
+        }
+
     }
 
     public static void main(String[] args){
-
+        final int WIDTH = 450, HEIGHT = 250;
+        JColorFrame f = new JColorFrame();
+        f.setSize(WIDTH, HEIGHT);
+        f.setVisible(true);
+        f.setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 }
